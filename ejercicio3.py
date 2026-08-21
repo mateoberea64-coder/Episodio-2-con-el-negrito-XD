70d459a7edba8ab2b7d179f14b6b74972cc33236
ElMateitoPapu <mateoberea64@gmail.com>
2026-08-21 15:29:02 +0000

    Tipo de variables MATEMATICA, con el negrito

---
+++ ejercicio3.py
@@ -0,0 +1,31 @@
+numero1 = int(input("ingrese el primer numero: "))
+numero2 = int(input("ingrese el segundo numero: "))
+
+multiplicacion = numero1 * numero2
+
+print ("Tecnicamente es:", multiplicacion )
+
+numero1 = int(input("ingrese el primer numero: "))
+numero2 = int(input("ingrese el segundo numero: "))
+
+suma = numero1 + numero2
+
+print ("Tecnicamente es:", suma )
+
+numero1 = int(input("ingrese el primer numero: "))
+numero2 = int(input("ingrese el segundo numero: "))
+
+multiplicacion = numero1 * numero2
+
+print ("Tecnicamente es:", multiplicacion )
+print ("Tecnicamente es:", suma )
+
+numero1 = int(input("ingrese el primer numero: "))
+numero2 = int(input("ingrese el segundo numero: "))
+
+multiplicacion = numero1 * numero2
+
+print ("Tecnicamente es:", multiplicacion )
+
+
+
