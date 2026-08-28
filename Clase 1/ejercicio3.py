a8562f8c27d923b0885207b931fd2ad2afff28ed
ElMateitoPapu <mateoberea64@gmail.com>
2026-08-28 11:50:17 +0000

    ejercicio3.py con el negritoooo XD

--- Clase 1/ejercicio3.py
+++ Clase 1/ejercicio3.py
@@ -15,17 +15,16 @@ print ("Tecnicamente es:", suma )
 numero1 = int(input("ingrese el primer numero: "))
 numero2 = int(input("ingrese el segundo numero: "))
 
-multiplicacion = numero1 * numero2
+resta = numero1 - numero2
 
-print ("Tecnicamente es:", multiplicacion )
-print ("Tecnicamente es:", suma )
+print ("Tecnicamente es:", resta )
 
 numero1 = int(input("ingrese el primer numero: "))
 numero2 = int(input("ingrese el segundo numero: "))
 
-multiplicacion = numero1 * numero2
+divicion = numero1 / numero2
 
-print ("Tecnicamente es:", multiplicacion )
+print ("Tecnicamente es:", divicon )
 
 
 
